ef1c9870b6053c467a23001d8fca4a4393cdfa1d
shunsuke-mizoguchi <shunsuke.m.1993@gmail.com>
2026-08-14 19:14:15 +0900

    feat: 一般ユーザーログイン機能を実装 #4

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * リクエストを許可するか
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * バリデーションルール
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => [
+                'required',
+                'string',
+                'email',
+            ],
+
+            'password' => [
+                'required',
+                'string',
+            ],
+        ];
+    }
+
+    /**
+     * エラーメッセージ
+     */
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'メールアドレスを入力してください',
+            'email.email' => 'メールアドレスはメール形式で入力してください',
+            'password.required' => 'パスワードを入力してください',
+        ];
+    }
+}
